30eb53dac0c13d96c2a23f6df2d53ce8180f47a9
NickLitharis <p19lith@ionio.gr>
2023-06-20 13:00:29 +0300

    multiple file conversion added

--- make.sh
+++ make.sh
@@ -1,47 +1,45 @@
 #!/bin/bash
 
-BUILDDIR="pdf"
-AUTHOR_FILE="report.yaml"
+BUILDDIR="build"
+AUTHOR_FILE="template/report.yaml"
 
+if [ "$1" = "src" ] && [ -d "$1" ]; then
+    INPUT_DIR="$1"
 
-# Check if the input file is provided
-if [ -z "$1" ]; then
-    echo "Usage: $0 <input.md>"
-    exit 1
+    # Check if the directory is empty
+    if [ -z "$(ls -A "$INPUT_DIR")" ]; then
+        echo "The directory $INPUT_DIR is empty."
+        exit 1
+    fi
+
+    # Create a temporary Markdown file
+    TEMP_FILE="$BUILDDIR/temp.md"
+
+    # Combine all Markdown files in the directory into the temporary file
+    cat "$AUTHOR_FILE" > "$TEMP_FILE"
+    echo -e "\n\n" >> "$TEMP_FILE"
+    for file in $INPUT_DIR/*.md; do
+        cat $file >> $TEMP_FILE
+        echo -e "\n\n" >> $TEMP_FILE
+    done
+
 fi
-
-# Get the input file from the command-line argument
-INPUT_FILE="$1"
-
-# Create a temporary Markdown file
-TEMP_FILE="$BUILDDIR/$(basename "$INPUT_FILE" .md)-temp.md"
-
-# Copy the contents of the author.yaml file to the temporary file
-cat "$AUTHOR_FILE" > "$TEMP_FILE"
-
-# Add two new lines
-echo -e "\n\n" >> "$TEMP_FILE"
-
-# Append the contents of the input file to the temporary file
-cat "$INPUT_FILE" >> "$TEMP_FILE"
-
-# Extract the file name without extension
-FILENAME=$(basename "$INPUT_FILE" .md)
 
 # Create the build directory if it doesn't exist
 mkdir -p "$BUILDDIR"
 
 # Check if Calibri font is installed
-if fc-list : file | grep -i "calibri.ttf" >/dev/null; then
-    FONT_OPTION="--variable=mainfont:Calibri"
+if fc-list : file | grep -i "arial.ttf" >/dev/null; then
+    FONT_OPTION="--variable=mainfont:Arial"
 else
-    FONT_OPTION="--variable=mainfont:sans-serif"
+    FONT_OPTION=""
 fi
 
 # Check if a file with the same name already exists in the build directory and delete it
-if [ -f "$BUILDDIR/$FILENAME.pdf" ]; then
-    rm "$BUILDDIR/$FILENAME.pdf"
+if [ -f "$BUILDDIR/final-report.pdf" ]; then
+    rm "$BUILDDIR/final-report.pdf"
 fi
+
 
 # Generate the PDF using Pandoc and the YAML metadata
 pandoc "$TEMP_FILE" \
@@ -55,11 +53,13 @@ pandoc "$TEMP_FILE" \
   --variable=mainfontoptions:"Scale=1" \
   --variable=justify:true \
   --toc \
-  --output="$BUILDDIR/$FILENAME.pdf" \
+  --output="$BUILDDIR/final-report.pdf" \
   --pdf-engine=xelatex
 
+
+
 if [ $? -eq 0 ]; then
-    echo "PDF generated: $BUILDDIR/$FILENAME.pdf"
+    echo "PDF generated: $BUILDDIR/final-report.pdf"
 else
     echo "Error generating PDF."
 fi
